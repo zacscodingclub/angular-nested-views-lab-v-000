993162c9c445c909a047a89926107c30f2ecccdf
zacscodingclub <zbaston@gmail.com>
2016-07-31 17:30:20 -0500

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,21 @@
 angular
-    .module('app', ['']);
+    .module('app', ['ui.router'])
+    .config(function($stateProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'views/home.html'
+        })
+        .state('home.notifications', {
+          url: 'notifications',
+          templateUrl: 'views/home/notifications.html'
+        })
+        .state('home.timeline', {
+          url: 'timeline',
+          templateUrl: 'views/home/timeline.html'
+        })
+        .state('home.user', {
+          url: 'user',
+          templateUrl: 'views/home/user.html'
+        })
+    });
